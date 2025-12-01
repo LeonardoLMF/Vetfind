0810b5ae60239696df525a9f5d016131bc7a7a78
LeonardoLMF <leonardo.lmf27@gmail.com>
2025-12-01 08:08:10 -0300

    project start

---
+++ vetfind/src/main/java/com/leo/vetfind/VetfindApplication.java
@@ -0,0 +1,13 @@
+package com.leo.vetfind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetfindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetfindApplication.class, args);
+	}
+
+}
---
+++ vetfind/src/test/java/com/leo/vetfind/VetfindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.vetfind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetfindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
