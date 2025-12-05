289e1bb7053a339186ed023b41bd8050ed58fd4e
LeonardoLMF <leonardo.lmf27@gmail.com>
2025-12-05 14:26:41 -0300

    refactor: correção das anotações lombok nos dtos

--- vetfind/src/main/java/com/leo/vetfind/dto/usuario/CadastroUsuarioResponseDTO.java
+++ vetfind/src/main/java/com/leo/vetfind/dto/usuario/CadastroUsuarioResponseDTO.java
@@ -1,9 +1,16 @@
 package com.leo.vetfind.dto.usuario;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CadastroUsuarioResponseDTO {
     //não passar a senha pois é sensivel
 
-    private String id;
+    private Long id;
     private String email;
     private String telefone;
     private String tipoUsuario;
